c360f7da9f2e36f8dd9567c78861d19197dcf62b
Witek <witek.nowogorski@gmail.com>
2023-03-16 14:55:30 +0100

    login page created

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class League(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     league_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
 
--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create_league(response):
+    if response.method == "POST":
+        form = (response.POST)
