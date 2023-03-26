026957d10d03ac7c0fe2c33a90284c966aa02f1f
Witek <witek.nowogorski@gmail.com>
2023-03-26 11:01:52 +0200

    league table properly sorted

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,4 @@
+import self as self
 from django import forms
 from django.contrib.auth.models import User
 from .models import League, Team, Match, Player
@@ -5,26 +6,26 @@ from .models import League, Team, Match, Player
 class CreateLeagueForm(forms.ModelForm):
     class Meta:
         model = League
-        fields = ['name',]
+        fields = ('name',)
         labels = {'name' : 'Name'}
 
 class CreateTeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ['name', 'shorthand']
+        fields = ('name', 'shorthand')
 
 class CreatePlayerForm(forms.ModelForm):
     class Meta:
         model = Player
-        fields = ['first_name', 'second_name', 'position']
+        fields = ('first_name', 'second_name', 'position')
 
 class EditMatchForm(forms.ModelForm):
     class Meta:
         model = Match
-        fields = ['home_team_result', 'away_team_result']
-        labels = {'home_team_result' : 'Home team result', 'away_team_result' : 'Away team result'}
+        fields = ('home_team_result', 'away_team_result')
+        labels = {'home_team_result' : '', 'away_team_result' : 'Away team result'}
 
 class EditLeagueForm(forms.ModelForm):
     class Meta:
         model = League
-        fields = ['name', 'promoted_teams', 'relegated_teams']
+        fields = ('name', 'promoted_teams', 'relegated_teams')
--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from operator import itemgetter
 
 register = template.Library()
 @register.filter(name='mul')
@@ -8,3 +9,7 @@ def mul(value, arg):
 @register.filter(name='div')
 def div(value, arg):
     return value - arg
+
+@register.filter
+def sortleague(value):
+    return reversed(sorted(value, key=lambda k: (k.points, k.goals_scored-k.goals_lost, k.goals_scored)))
